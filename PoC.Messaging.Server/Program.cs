d515350af38d7e229bf446a30a6e40f92ef5347e
Stefik, Ondrej <ondrej.stefik@gmail.com>
2023-10-29 14:45:04 +0100

    init solution with signalr

---
+++ PoC.Messaging.Server/Program.cs
@@ -0,0 +1,50 @@
+using PoC.Messaging.Server.Hubs;
+
+namespace PoC.Messaging.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddSignalR();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+            app.MapHub<ChatHub>("/hub");
+
+            app.UseCors((settings) =>
+            {
+                settings.AllowCredentials();
+                settings.AllowAnyHeader();
+                settings.AllowAnyMethod();
+                settings.SetIsOriginAllowed((_) =>
+                {
+                    return true;
+                });
+            });
+
+            app.Run();
+        }
+    }
+}
---
+++ PoC.Messaging/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PoC.Messaging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var drawer = new Drawer(5, 5);
+            //drawer.Draw();
+
+            Console.WriteLine("Hello World!");
+
+            //Console.ReadLine();
+        }
+    }
+}
